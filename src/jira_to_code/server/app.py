9b68299c2d8959ded15712c4638175d8db942619
Vaishnavi Waghmare <vsw985@gmail.com>
2026-04-08 02:34:47 +0530

    fixed port

--- src/jira_to_code/server/app.py
+++ src/jira_to_code/server/app.py
@@ -13,7 +13,7 @@ app = create_web_interface_app(env, JiraCodeAction, JiraCodeObservation)
 def serve():
     """Entry point for the OpenEnv validator to run the server."""
     port = int(os.environ.get("PORT", 7860))
-    uvicorn.run("src.jira_to_code.server.app:app", host="0.0.0.0", port=8000)
+    uvicorn.run("src.jira_to_code.server.app:app", host="0.0.0.0", port=port)
 
 @app.get("/")
 def root():
